98cf0985fc2ae3823d294f91f32b5e0781365960
Sawhsiv <102612514+Sawhsiv@users.noreply.github.com>
2024-06-15 22:17:44 +0530

    Add files via upload

---
+++ basic program on class.py
@@ -0,0 +1,12 @@
+class Ez:
+    def __init__(self,nm,ag,gn):
+        self.name = nm
+        self.age = ag
+        self.gender = gn
+
+
+st1 =Ez("vishwas",21,'m')
+
+print(st1.name,st1.age,st1.gender)
+
+print(type(st1))
