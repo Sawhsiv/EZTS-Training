583cd86702f7bea9460002158754a774901a2402
Sawhsiv <102612514+Sawhsiv@users.noreply.github.com>
2024-06-15 21:59:43 +0530

    Add files via upload
    program to identify prime numbers

---
+++ prime_number_identifier.py
@@ -0,0 +1,16 @@
+m = int(input("Enter msg "))
+flag = 0
+if m<1:
+    flag = 1
+elif m == 1:
+    flag = 0
+else:
+    for i in range(2,(m//2)+1):
+        if m%i == 0:
+            flag = 1
+            break
+    
+if flag == 0:
+    print("Valid Messgae")
+else:
+    print("invalid Message")
